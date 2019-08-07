11afb63683717859889cc8a92e8d9c102c4f5a8a
JhChoy <jchoyeclipse@gmail.com>
2019-08-07 16:59:30 +0900

    fix: wiring

--- app/app.js
+++ app/app.js
@@ -1,11 +1,22 @@
+require('dotenv').config();
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(process.env.MONGO_URI, {useCreateIndex: true, useNewUrlParser: true})
+  .then(() => console.log('Successfully connected to mongodb'))
+  .catch(e => console.error(e));
+
+require('./subscriber').subscriber();
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const gameRouter = require('./routes/game');
 
 const app = express();
 
@@ -14,13 +25,14 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.static('public'));
+// Body-parser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/game', gameRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
