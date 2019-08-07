5a946301e0e3bdf3d0075b734961953276f9c221
JhChoy <jchoyeclipse@gmail.com>
2019-08-07 10:41:36 +0900

    feat: make index page

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const GameList = require('../models/gameList');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  GameList.find({}).then((dBGames) => {
+    const games = dBGames.map((game) => {
+      return {
+        address: game.address,
+        host: game.host,
+      }
+    });
+    res.render('index', { title: 'Bomberman', games: JSON.stringify(games)});
+  });
 });
 
 module.exports = router;
